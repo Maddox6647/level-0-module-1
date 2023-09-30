e2a1b8fe648afd84422df4d9395f8cd49d73c795
league <maddoxseul@sbsd.net>
2023-09-29 18:08:41 -0700

    badgers ate my bananas

--- _04_int/_1_riddler/riddler.py
+++ _04_int/_1_riddler/riddler.py
@@ -11,3 +11,24 @@
 * After all the riddles have been asked, tell the user how many they got
   correct
 """
+from tkinter import messagebox, simpledialog, Tk
+
+if __name__ == "__main__":
+    window = Tk()
+    window.withdraw()
+    shrek = simpledialog.askstring(title='yay', prompt='what three riddles do you want: ogre, muppit, or sus?')
+
+    if shrek == 'ogre':
+        donkey = simpledialog.askstring(title='mommy', prompt='what do you call an ogre that is the size of a tree but is from a movie?')
+        if donkey == 'SHREK':
+            messagebox.showinfo(title='er', message='U WIN!!!!!!')
+        else:
+            messagebox.showinfo(title='ballz', message='U FAILURE')
+
+    elif shrek == 'muppit':
+        MATTHEW = simpledialog.askstring(title='boo', prompt=' what is a muppit that is as dumb as a frog but as big as an arm')
+        if MATTHEW == 'KERMIT THE FROG':
+            messagebox.showinfo(title='shrek', message='lol that just luck')
+
+
+
